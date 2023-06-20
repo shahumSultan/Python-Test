d20e663d084b2fdf4a981286a5777c46784eece8
Shahum <shahum@exarta.com>
2023-06-20 11:38:37 +0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## How to Run
+# How to Run the Quix
+
+## Steps
+- Clone the repo
+- Start with the questions from the first to last
+- Your time will be noted
+- Your progress is going to noted in this quiz
+
+# NOT TO USE GPT OR GOOGLE
--- question-one.py
+++ question-one.py
@@ -9,17 +9,3 @@
 #Constraints => 1 ­≤ n ≤ 100
 
 ######code below this line######
-def NumberCheker(n):
-    if ((n%2) == 0) and (n >= 2 and n <= 5):
-        print("Even and in between range 2 to 5")
-    elif ((n%2) == 0) and (n >= 6 and n <= 20):
-        print("Even and in between range 6 to 20")
-    elif ((n%2) == 0) and (n > 20):
-        print("Even and greater than 20")
-    else:
-        print("Odd")
-    
-
-if __name__ == '__main__':
-    n = int(input().strip())
-    NumberCheker(n)
