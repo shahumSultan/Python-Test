a69985de79e56cf0bb9ee697af3e8c4ceb8780bb
Shahum <shahum@exarta.com>
2023-06-20 12:10:55 +0500

    added question 3

---
+++ question-three.py
@@ -0,0 +1,17 @@
+#Task (Swap-Case)
+#Given an string, , perform the following actions:
+#   - Swap the cases, convert all lowercase to uppercase letters and vice versa
+#   - Print the final output
+
+#Input Format => a single line containing a string 's'
+#Constraints => 0 ­< n < 1000
+
+######code below this line######
+def swap_case(s):
+    return ()
+
+######do not change these lines, only add functionalites if needed######
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
